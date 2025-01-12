215e94fbc923c71033b5cf018c524ffe4c03cec9
thriftymumuk <thriftymumkatie@gmail.com>
2025-01-12 22:04:33 +0000

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,6 +1,7 @@
-import * as React from "react"
+"use client";
 
-import { cn } from "@/lib/utils"
+import * as React from "react"
+import { cn } from "../../lib/utils"
 
 const Card = React.forwardRef
   HTMLDivElement,
@@ -44,18 +45,6 @@ const CardTitle = React.forwardRef
 ))
 CardTitle.displayName = "CardTitle"
 
-const CardDescription = React.forwardRef
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => (
-  <p
-    ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
-    {...props}
-  />
-))
-CardDescription.displayName = "CardDescription"
-
 const CardContent = React.forwardRef
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
@@ -64,16 +53,4 @@ const CardContent = React.forwardRef
 ))
 CardContent.displayName = "CardContent"
 
-const CardFooter = React.forwardRef
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
-    {...props}
-  />
-))
-CardFooter.displayName = "CardFooter"
-
-export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter }
+export { Card, CardHeader, CardTitle, CardContent }
