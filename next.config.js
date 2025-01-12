f886ce48d4d2db8960161fbc2f224f13cb695ddd
thriftymumuk <thriftymumkatie@gmail.com>
2025-01-12 21:34:33 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/emergency_fund_calc'
+  basePath: '/emergency_fund_calc',  // Your repo name
+  images: {
+    unoptimized: true,
+  }
 }
 
 module.exports = nextConfig
