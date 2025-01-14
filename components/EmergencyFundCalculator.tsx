317f7e4d1f1f9dd472580bfcdc3f8a18fbe65545
thriftymumuk <thriftymumkatie@gmail.com>
2025-01-14 14:18:57 +0000

    Update EmergencyFundCalculator.tsx

--- components/EmergencyFundCalculator.tsx
+++ components/EmergencyFundCalculator.tsx
@@ -6,6 +6,7 @@ import { Card, CardHeader, CardTitle, CardContent } from './ui/card';
 const EmergencyFundCalculator = () => {
   const [expenses, setExpenses] = useState({
     mortgage: 0,
+    childcare:0,
     utilities: 0,
     food: 0,
     transport: 0,
