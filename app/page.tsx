8f9163ee1fcd65a55ade20144028c10346599dd5
thriftymumuk <thriftymumkatie@gmail.com>
2025-01-14 17:13:45 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import EmergencyFundCalculator from '../components/EmergencyFundCalculator'
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-4 bg-gray-50">
+    <main className="min-h-screen pt-2 bg-gray-50">
       <EmergencyFundCalculator />
     </main>
   )
